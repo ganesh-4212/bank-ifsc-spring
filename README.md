300b1826c0e3c6dd8df69bfb32d15fba5be241a0
Ganesh Poojary <ganesh4212@gmail.com>
2019-11-12 21:43:39 +0530

    Added api doc.

---
+++ README.md
@@ -0,0 +1,9 @@
+### Indian Bank IFSC Search
+ - Java Spring boot
+ - Postgres DB
+ - JWT authentication
+#### Demo url = https://fast-depths-77312.herokuapp.com
+
+Rest API doc : 
+curl script : 
+
--- src/main/java/com/ganesh/bankifsc/controller/BankDetailsController.java
+++ src/main/java/com/ganesh/bankifsc/controller/BankDetailsController.java
@@ -19,11 +19,48 @@ public class BankDetailsController {
 	@Autowired
 	BankDetailsService bankDetailsService;
 
+	/**
+	 * @apiVersion 1.0.0
+	 * @api {get} /branches/:ifscCode Get Bank details by IFSC code.
+	 * @apiName GetByIFSCCode
+	 * @apiGroup Bank Details
+	 * @apiParam {String} ifscCode Bank Branch IFSC code.
+	 *
+	 * @apiSuccess {Object} BankDetailsDto Bank branch details.
+	 * @apiSuccess {Number} BankDetailsDto.id Bank id.
+	 * @apiSuccess {String} BankDetailsDto.name Bank name.
+	 * @apiSuccess {String} BankDetailsDto.ifsc Bank branch ifsc code.
+	 * @apiSuccess {String} BankDetailsDto.branch Bank branch name.
+	 * @apiSuccess {String} BankDetailsDto.address Bank branch address.
+	 * @apiSuccess {String} BankDetailsDto.city Bank branch city.
+	 * @apiSuccess {String} BankDetailsDto.district Bank branch district.
+	 * @apiSuccess {String} BankDetailsDto.state Bank branch state.
+	 */
 	@GetMapping("/{ifscCode}")
 	Optional<BankDetailsDto> getByIFSCCode(@PathVariable("ifscCode") String ifscCode) {
 		return bankDetailsService.getByIFSCCode(ifscCode);
 	}
 
+	/**
+	 * @apiVersion 1.0.0
+	 * @api {get} /branches Get All Bank branch details (Paginated).
+	 * @apiName GetByBankNameAndCity
+	 * @apiGroup Bank Details
+	 * @apiParam {String} bankName Bank name.
+	 * @apiParam {String} city Bank branch city.
+	 * @apiParam {Number} page Pagination page index (Zero based).
+	 * @apiParam {Number} size Number of records per request.
+	 *
+	 * @apiSuccess {Object[]} BankDetailsDto Bank branch details.
+	 * @apiSuccess {Number} BankDetailsDto.id Bank id.
+	 * @apiSuccess {String} BankDetailsDto.name Bank name.
+	 * @apiSuccess {String} BankDetailsDto.ifsc Bank branch ifsc code.
+	 * @apiSuccess {String} BankDetailsDto.branch Bank branch name.
+	 * @apiSuccess {String} BankDetailsDto.address Bank branch address.
+	 * @apiSuccess {String} BankDetailsDto.city Bank branch city.
+	 * @apiSuccess {String} BankDetailsDto.district Bank branch district.
+	 * @apiSuccess {String} BankDetailsDto.state Bank branch state.
+	 */
 	@GetMapping()
 	List<BankDetailsDto> getByBankNameAndCity(@RequestParam("bankName") Optional<String> bankName,
 			@RequestParam("city") Optional<String> city, @RequestParam("page") int page,
